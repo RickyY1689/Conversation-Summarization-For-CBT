1ec01c00f61d866809ca297caca7452f8b853e41
RickyY1689 <richard.jr.yang@gmail.com>
2022-11-17 15:36:45 -0500

    cleaned up pipeline code

--- gpt3_pipeline.py
+++ gpt3_pipeline.py
@@ -1,4 +1,5 @@
 import os
+import json
 import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -13,21 +14,28 @@ prompt_seq = ["Summarize this text message conversation between BOT and USER:",
               "Convert this text from third to first person:",
               "Convert from first person to second person:"]
 
-for convo in convos:
+res_dict = {}
+
+for i, convo in enumerate(convos):
     data = convo
-    print("------------ New Convo: ------------")
-    print(data)
-    for prompt in prompt_seq:
-        response = openai.Completion.create(
-        model="text-davinci-002",
-        prompt=f"{prompt}\n\n{data}",
-        temperature=0.7,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-        )
+    res_dict[i] = {"data": data}
+    print(f"------------ Convo Number #{i+1} ------------")
+    for j, prompt in enumerate(prompt_seq):
+        response = openai.Completion.create(model="text-davinci-002",
+                                            prompt=f"{prompt}\n\n{data}",
+                                            temperature=0.7,
+                                            max_tokens=256,
+                                            top_p=1,
+                                            frequency_penalty=0,
+                                            presence_penalty=0)
         
         data = response.choices[0].text.replace('\n', '')
+        res_dict[i][j] = {}
+        res_dict[i][j]["prompt"] = prompt
+        res_dict[i][j]["result"] = data
         print(f"{prompt}")
         print(f"{data}")
+        
+# Directly from dictionary
+with open('json_data.json', 'w') as outfile:
+    json.dump(res_dict, outfile)
--- research/Prompt-Engineering-Deep-Dive.md
+++ research/Prompt-Engineering-Deep-Dive.md
@@ -2,11 +2,26 @@
 
 ### Resources
 
-[Medium Article on GPT3 Zero Shot Prompt Engineering](https://medium.com/nerd-for-tech/prompt-engineering-the-career-of-future-2fb93f90f117)
+- Articles
 
-[Riku AI Prompt  Engineering Article](https://blog.riku.ai/what-is-prompt-engineering/)
+  - [Medium Article on GPT3 Zero Shot Prompt Engineering](https://medium.com/nerd-for-tech/prompt-engineering-the-career-of-future-2fb93f90f117)
+
+  - [Riku AI Prompt  Engineering Article](https://blog.riku.ai/what-is-prompt-engineering/)
+
+- Papers on Prompt Engineering
+
+  - https://www.semanticscholar.org/paper/Prompt-scoring-system-for-dialogue-summarization-Prodan-Pelican/0d08572c4ec1974478d6ba8467b6e7be30867a0d
+  - https://www.semanticscholar.org/paper/An-Information-theoretic-Approach-to-Prompt-Without-Sorensen-Robinson/d53e70d834243d3d8d4b621c0c52dfec26081155
+  - https://www.semanticscholar.org/paper/Design-Guidelines-for-Prompt-Engineering-Generative-Liu-Chilton/bfef309db9902dbcac34cae7a54670f9513ef6f0
+
+- Papers on Text Summarization
+
+  - https://www.semanticscholar.org/paper/Automatic-text-summarization%3A-A-comprehensive-El-Kassas-Salama/cbb8faf1051d858b4b0426742fdbbd0f104833ea
+  - https://www.semanticscholar.org/paper/Natural-Language-Processing-(NLP)-based-Text-A-Awasthi-Gupta/07d8b6e6fb2c842b3f28fddeeb9c0300cc5d0951
+  - https://www.hindawi.com/journals/cin/2022/7132226/
 
 ### Unsorted Notes
 
 - When playing around with hyperparameters, if you're messing with temperature leave top-k alone and vice versa
 - 
+
